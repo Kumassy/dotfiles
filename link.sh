f82a1db85a870a3ba869407599a08c11556c38c1
Kumassy <kumassyii@gmail.com>
2019-01-23 00:39:53 +0900

    add: tmux, fish, fisher

--- link.sh
+++ link.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/
+ln -sf ~/dotfiles/.config/fish ~/.config/fish
+ln -sf ~/dotfiles/.config/fisher ~/.config/fisher
