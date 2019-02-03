8deaf7aa7888d2c1a6574db38bec498bf2240652
Kumassy <kumassyii@gmail.com>
2019-02-03 18:28:29 +0900

    fix: fisher installation

--- link.sh
+++ link.sh
@@ -4,4 +4,3 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/
 ln -sf ~/dotfiles/.config/fish ~/.config/fish
-ln -sf ~/dotfiles/.config/fisher ~/.config/fisher
