643865f933fb9c0a606c87a4fb4755d65284395c
Kumassy <kumassyii@gmail.com>
2019-04-06 21:16:10 +0900

    mv: powerline-shell -> powerline

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
-function fish_prompt
-    powerline-shell --shell bare $status
-end
+powerline-daemon -q
+set fish_function_path $fish_function_path "/usr/lib/python2.7/site-packages/powerline/bindings/fish"
+powerline-setup
+
+# function fish_prompt
+#     powerline-shell --shell bare $status
+# end
 
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
